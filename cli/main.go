ebf694f6ddc9374a3d27311b1af1afc9b3b3f32f
sanghg <shg@disanbo.com>
2018-11-08 11:17:37 +0800

    调整main.go中引用插件的路径，由 gitlab.33.cn/chain33/chain33/ 改为 gitlab.33.cn/chain33/

--- cli/main.go
+++ cli/main.go
@@ -4,9 +4,9 @@ package main
 
 import (
 	_ "gitlab.33.cn/chain33/bityuan/plugin"
-	_ "gitlab.33.cn/chain33/chain33/plugin"
-	_ "gitlab.33.cn/chain33/chain33/system"
-	"gitlab.33.cn/chain33/chain33/util/cli"
+	_ "gitlab.33.cn/chain33/plugin"
+	_ "gitlab.33.cn/chain33/system"
+	"gitlab.33.cn/chain33/util/cli"
 )
 
 func main() {
