1d6ac392511f041b88bc8a573ebc26aba5266b4d
sanghg <shg@disanbo.com>
2018-11-08 11:47:51 +0800

    修复cli无法编译通过的问题

--- cli/main.go
+++ cli/main.go
@@ -6,7 +6,7 @@ import (
 	_ "gitlab.33.cn/chain33/bityuan/plugin"
 	_ "gitlab.33.cn/chain33/chain33/system"
 	"gitlab.33.cn/chain33/chain33/util/cli"
-	_ "gitlab.33.cn/chain33/plugin"
+	//_ "gitlab.33.cn/chain33/plugin"
 )
 
 func main() {
