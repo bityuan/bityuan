4f86abc79aa99cb6858f9f88477b2a00fbadb9e9
kingwang <kingwang@kingwangdeiMac.local>
2018-12-05 15:16:29 +0800

    update init

---
+++ plugin/dapp/init/init.go
@@ -0,0 +1,10 @@
+package init
+
+import (
+	_ "github.com/33cn/plugin/plugin/dapp/hashlock"
+	_ "github.com/33cn/plugin/plugin/dapp/retrieve"
+	_ "github.com/33cn/plugin/plugin/dapp/ticket"
+	_ "github.com/33cn/plugin/plugin/dapp/token"
+	_ "github.com/33cn/plugin/plugin/dapp/trade"
+	_ "github.com/bityuan/bityuan/plugin/dapp/hello"
+)
---
+++ plugin/init.go
@@ -0,0 +1,8 @@
+package plugin
+
+import (
+	_ "github.com/bityuan/bityuan/plugin/consensus/init"
+	_ "github.com/bityuan/bityuan/plugin/crypto/init"
+	_ "github.com/bityuan/bityuan/plugin/dapp/init"
+	_ "github.com/bityuan/bityuan/plugin/store/init"
+)
