c7efc7885b7d327d8d20ece4968a6c867069a0c1
mdj33 <travis@travis-ci.org>
2018-12-11 20:10:46 +0800

    linter err

--- plugin/init.go
+++ plugin/init.go
@@ -1,7 +1,7 @@
 package plugin
 
 import (
-	_ "github.com/bityuan/bityuan/plugin/consensus/init"
+	_ "github.com/bityuan/bityuan/plugin/consensus/init" //register consensus init package
 	_ "github.com/bityuan/bityuan/plugin/crypto/init"
 	_ "github.com/bityuan/bityuan/plugin/dapp/init"
 	_ "github.com/bityuan/bityuan/plugin/store/init"
