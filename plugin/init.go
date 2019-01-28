36a3231daef8820107060700e1659be74be3dbf4
kingwang <kingwang@kingwangdeiMac.local>
2019-01-28 14:34:08 +0800

    update mempool config

--- plugin/init.go
+++ plugin/init.go
@@ -4,5 +4,6 @@ import (
 	_ "github.com/bityuan/bityuan/plugin/consensus/init" //register consensus init package
 	_ "github.com/bityuan/bityuan/plugin/crypto/init"
 	_ "github.com/bityuan/bityuan/plugin/dapp/init"
+	_ "github.com/bityuan/bityuan/plugin/mempool/init"
 	_ "github.com/bityuan/bityuan/plugin/store/init"
 )
