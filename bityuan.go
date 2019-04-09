2bd5bafb641d85d301352b10101eaf094a8c9efb
liuyuhang <liuyuhang203@sina.com>
2019-04-09 14:53:20 +0800

    modify kvmvcc sync

--- bityuan.go
+++ bityuan.go
@@ -2,7 +2,7 @@ package main
 
 var bityuan = `
 TestNet=false
-version="6.1.0.0403"
+version="6.1.0.0404"
 [blockchain]
 defCacheSize=128
 maxFetchBlockNum=128
@@ -110,8 +110,9 @@ returnAddr="1AH9HRd4WBJ824h9PP1jYpvRZ4BSA4oN6Y"
 count=4733
 
 [store]
-name="mavl"
+name="kvmvccmavl"
 driver="leveldb"
+storedbVersion="2.0.0"
 
 [wallet]
 minFee=100000
