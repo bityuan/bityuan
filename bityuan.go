967d75b2bc54e03526d6cd5cade6faee52d193af
vipwzw <vipwzw@vip.qq.com>
2020-03-25 20:15:23 +0800

    Update bityuan.go

--- bityuan.go
+++ bityuan.go
@@ -22,6 +22,17 @@ driver="leveldb"
 [p2p.sub.gossip]
 serverStart=true
 
+[p2p.sub.dht]
+seeds=["/ip4/116.62.14.25/tcp/13803/p2p/16Uiu2HAmTLe2UuhiEiXkMBzbMsEUSJSqRCrc2rDJou855amo1JGD",
+"/ip4/39.106.166.159/tcp/13803/p2p/16Uiu2HAmM7iiHwerHR63CpG71sd2Y3jmC6QrPuAT1hRPWL9wx4eB",
+"/ip4/47.106.114.93/tcp/13803/p2p/16Uiu2HAmAvHCoV1i9U3MHorQGZNRUhms1FXpd57g12fpovg3owRS",
+"/ip4/120.76.100.165/tcp/13803/p2p/16Uiu2HAmGoQ8Sp9Tk42ftV6TjrMaSjSNuYJ6xNKVoAw18AcNJ6dy",
+"/ip4/120.24.85.66/tcp/13803/p2p/16Uiu2HAkwfpLqNcPpCgv6Pfewk58mV4mKF9xQJJdNY392wFa5bGC",
+"/ip4/120.24.92.123/tcp/13803/p2p/16Uiu2HAkzabkVNN9WPu63R27i7449cAYLkT7FgumDyYUYa9j4fQ8",
+"/ip4/116.62.14.25/tcp/13803/p2p/16Uiu2HAmTLe2UuhiEiXkMBzbMsEUSJSqRCrc2rDJou855amo1JGD",
+"/ip4/39.106.166.159/tcp/13803/p2p/16Uiu2HAmM7iiHwerHR63CpG71sd2Y3jmC6QrPuAT1hRPWL9wx4eB",
+"/ip4/39.106.193.172/tcp/13803/p2p/16Uiu2HAm19JAcWxkryuYShj6RWn3TGP4Y4Er1DwFe7AC4TevbuU6"]
+
 #下一个版本改成: price 模式
 [mempool]
 name="price"
