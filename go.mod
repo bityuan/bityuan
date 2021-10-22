4b77a9cdfda85336f32cc51ed2bb9a6b06ac735f
suyanlong <592938410@qq.com>
2021-10-22 17:59:20 +0800

    1、Add github action 2、Add version control 3、Add Auto make release 4、Update chain33 pulgin version for latest master branch

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/33cn/chain33 v1.65.3-0.20211013024110-945de0b0c9c1
-	github.com/33cn/plugin v1.65.3-0.20211013065216-8d54779f1dc5
+	github.com/33cn/plugin v1.65.4-0.20211020073713-a0eade8bb670
 	github.com/stretchr/objx v0.2.0 // indirect
 )
---
+++ version/version.go
@@ -0,0 +1,24 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var (
+	Version   = "6.5.2"
+	GitCommit string
+	BuildTime string
+	// GoVersion system go version
+	GoVersion = runtime.Version()
+	// Platform info
+	Platform = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+)
+
+//GetVersion 获取版本信息
+func GetVersion() string {
+	if GitCommit != "" {
+		return Version + "-" + GitCommit
+	}
+	return Version
+}
