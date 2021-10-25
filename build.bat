0d627f50228cd4bb996102146aec941821ae0831
mdj33 <mdj33@disanbo.com>
2021-10-25 16:23:50 +0800

    add buildmode=exe to fix Error:export ordinal too large: 82159

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 go env -w CGO_ENABLED=1
-go build -o chain33.exe
-go build -o chain33-cli.exe github.com/33cn/plugin/cli
+go build -buildmode=exe -o chain33.exe
+go build -buildmode=exe -o chain33-cli.exe github.com/33cn/plugin/cli
