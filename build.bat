3829c6a1d3b679cc68a1600aa602ee76a8cffea3
suyanlong <592938410@qq.com>
2022-01-26 11:48:58 +0800

    Add windows build script

--- build.bat
+++ build.bat
@@ -1,3 +1,12 @@
+@echo on
+
+set BUILDTIME=%date:~3%-%time:~0,2%:%time:~3,2%:%time:~6,2%
+echo %BUILDTIME%
+
+for /F "delims=" %%i in ('git rev-parse --short HEAD') do ( set commitid=%%i)
+
+set BUILD_FLAGS="-X github.com/bityuan/bityuan/version.GitCommit=%commitid%  -X github.com/33cn/chain33/common/version.GitCommit=%commitid% -X github.com/bityuan/bityuan/version.BuildTime=%BUILDTIME% -w -s"
+
 go env -w CGO_ENABLED=1
-go build -buildmode=exe -o chain33.exe
-go build -buildmode=exe -o chain33-cli.exe github.com/33cn/plugin/cli
+go build  -ldflags  %BUILD_FLAGS% -v -o bityuan.exe github.com/bityuan/bityuan
+go build  -ldflags  %BUILD_FLAGS% -v -o bityuan-cli.exe github.com/bityuan/bityuan/cli
