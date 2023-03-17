ec30206c79ab27b784f87f0f0eeaa103615632fe
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2023-03-17 10:22:57 +0800

    add ForkIntrinsicGas fork

--- bityuan.go
+++ bityuan.go
@@ -343,6 +343,7 @@ ForkEVMFrozen=19900000
 ForkEVMTxGroup=19900000
 ForkEVMKVHash=19900000
 ForkEVMMixAddress=25200000
+ForkIntrinsicGas=25200000
 ForkEVMAddressInit=25770000
 
 [fork.sub.rollup]
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/33cn/chain33 v1.68.0
-	github.com/33cn/plugin v1.68.2
+	github.com/33cn/plugin v1.68.3-0.20230316112413-ee579c10a2c8
 )
